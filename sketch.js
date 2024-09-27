a8431b124f3d14f2e8d9fee6592833ca86c42a2e
Ryan-Iago <ryan.andre.prado@escola.pr.gov.br>
2024-09-27 10:29:23 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,31 @@
+let cor;
+let posicaoHorizontal;//x
+let posicaoVertical;//y
+
 function setup() {
-    createCanvas(600, 600);
-    background("black");
+  createCanvas(400, 400);
+  background(color(100,0,0));
+  cor = color(random(0,255),random(0,255),random(0,255));
+  posicaoHorizontal = 200
+  posicaoVertical = 200
+}
+
+function draw() {
+  fill(cor)
+  circle(posicaoHorizontal,posicaoVertical,50);
+  if(mouseX<posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal -1;
   }
-  
-  function draw() {
-    stroke("blue");
-    fill("red");
-  
-   if(mouseIsPressed){
-    rect(mouseX, mouseY, 20, 35);
-     
-   }
-  
-   }
+  if(mouseX>posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal +1;
+  }
+  if(mouseY<posicaoVertical){
+    posicaoVertical--;
+  }
+  if(mouseY>posicaoVertical){
+    posicaoVertical++;
+  }
+   if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+}
